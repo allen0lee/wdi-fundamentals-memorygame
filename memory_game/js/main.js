2d3e4b70eed6c8287e510e374582c4fcf12f32b1
allen0lee <allen0lee@gmail.com>
2019-03-21 23:03:05 +1100

    4th commit

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,23 @@
-var cards = ["queen", "queen", "king", "king"];
+//var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {rank: "queen",
+     suit: "hearts",
+     cardImage: "images/queen-of-hearts.png"
+    },
+    {rank: "queen",
+     suit: "diamonds",
+     cardImage: "images/queen-of-diamonds.png"
+    },   
+    {rank: "king",
+     suit: "hearts",
+     cardImage: "images/king-of-hearts.png"
+    },
+    {rank: "king",
+     suit: "diamonds",
+     cardImage: "images/king-of-diamonds.png"
+    }
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -11,9 +30,11 @@ var checkForMatch = function(){
 }
 
 var flipCard = function(cardId){
-
-     console.log("User flipped " + cards[cardId]);
-     cardsInPlay.push(cards[cardId]);
+     //console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
+     console.log("User flipped " + cards[cardId].rank);
+     console.log(cards[cardId].cardImage);
+     console.log(cards[cardId].suit);
+     cardsInPlay.push(cards[cardId].rank);
 
     if(cardsInPlay.length == 2){
     	checkForMatch();
@@ -26,11 +47,3 @@ var flipCard = function(cardId){
 flipCard(0);
 flipCard(2);
 
-/*
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-*/
